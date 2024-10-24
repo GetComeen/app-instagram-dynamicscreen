447694e29defacb151ded6698a891e023a19e7e2
Simon Cleriot <cleriotsimon@gmail.com>
2024-10-24 19:35:20 +0200

    Show error if account not working

--- src/Instagram/InstagramOptions.ts
+++ src/Instagram/InstagramOptions.ts
@@ -13,21 +13,28 @@ export default class InstagramOptionsModule extends SlideOptionsModule {
     const { h, ref, reactive, computed, watchEffect, watch } = vue;
 
     let isAccountDataLoaded = ref(false)
+    let hasError = ref(false);
     const pages = reactive<any>([]);
 
+    context.setAccountPickerVisibility(true);
     context.watchAccountSelected("facebook-driver", (accountId: number | undefined) => {
       isAccountDataLoaded.value = false;
+      hasError.value = false;
       console.log(accountId, 'onchange')
       if (accountId === undefined) {
         pages.value = [];
         isAccountDataLoaded.value = true;
       } else {
         this.context.getAccountData("facebook-driver", "instagram_accounts").then((data: any) => {
-          pages.value = Object.keys(data).map((key) => {
-            return { 'key': key, 'name': data[key] };
-          });;
-          isAccountDataLoaded.value = true;
-          console.log('account data successfully fetched', pages.value)
+          if (data) {
+            pages.value = Object.keys(data).map((key) => {
+              return { 'key': key, 'name': data[key] };
+            });;
+            isAccountDataLoaded.value = true;
+            console.log('account data successfully fetched', pages.value)
+          } else {
+            hasError.value = true;
+          }
         })
       }
     })
@@ -46,12 +53,15 @@ export default class InstagramOptionsModule extends SlideOptionsModule {
     return () => [
       h(LoadingBlock, {
         class: "h-full",
-        loading: !isAccountDataLoaded.value
+        loading: !hasError.value && !isAccountDataLoaded.value
       }, [
         h("div", {
           class: "space-y-5"
         }, [
-          h(FieldsRow, {}, [
+          hasError.value && h("div", {
+            class: 'text-red-600 mb-6',
+          }, this.t('modules.instagram.options.error')),
+          !hasError.value && (h(FieldsRow, {}, [
             h(Field, { class: 'flex-1', label: this.t("modules.instagram.options.page_count")}, [
               h(NumberInput, { min: 0, max: 100, default: 1, ...update.option("pageNumber", { default: 1 }) })
             ]),
@@ -64,7 +74,7 @@ export default class InstagramOptionsModule extends SlideOptionsModule {
               placeholder: this.t('modules.instagram.options.account-picker.placeholder'),
               ...update.option("pageId")
             })
-          ]),
+          ])),
         ])
       ])
     ]
